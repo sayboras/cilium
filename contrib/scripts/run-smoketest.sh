f7df39bda4b129bf73b33005b1fc158af9917d61
Tam Mach <sayboras@yahoo.com>
2020-09-12 16:42:10 +1000

    add script to run smoke test locally

---
+++ contrib/scripts/run-smoketest.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+kind delete cluster || true
+kind create cluster --wait 5m --config=.github/kind-config.yaml
+
+# Install cilium
+helm install cilium ./install/kubernetes/cilium \
+   --wait \
+   --namespace kube-system \
+   --set global.debug.enabled=true \
+   --set global.nodeinit.enabled=true \
+   --set global.kubeProxyReplacement=partial \
+   --set global.hostServices.enabled=false \
+   --set global.externalIPs.enabled=true \
+   --set global.nodePort.enabled=true \
+   --set global.hostPort.enabled=true \
+   --set config.bpfMasquerade=false \
+   --set config.ipam=kubernetes \
+   --set global.pullPolicy=Never \
+   --set global.prometheus.enabled=true \
+   --set global.operatorPrometheus.enabled=true \
+   --set global.hubble.enabled=true \
+   --set global.hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,http}"
+
+kubectl wait -n kube-system --for=condition=Ready --all pod --timeout=5m
+kubectl wait --for condition=Established crd/ciliumnetworkpolicies.cilium.io --timeout=5m
+kubectl apply -f examples/kubernetes/connectivity-check/connectivity-check.yaml
